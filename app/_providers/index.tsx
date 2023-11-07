c164fb54c195c5ad50aee90c2655f2d84c223f49
zmeyer44 <zmmeyer44@gmail.com>
2023-11-07 15:44:43 -0500

    provider file structure

---
+++ app/_providers/index.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { Toaster } from "sonner";
+import useRouteChange from "@/lib/hooks/useRouteChange";
+export function Providers({ children }: { children: React.ReactNode }) {
+  const handleRouteChange = (url: string) => {
+    const RichHistory = sessionStorage.getItem("RichHistory");
+    if (!RichHistory) {
+      sessionStorage.setItem("RichHistory", "true");
+    }
+  };
+  useRouteChange(handleRouteChange);
+  return (
+    <>
+      <Toaster richColors className="dark:hidden" />
+      <Toaster theme="dark" className="hidden dark:block" />
+      {children}
+    </>
+  );
+}
