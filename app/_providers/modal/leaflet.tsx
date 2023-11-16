4788eda26defd149ac716c388d54135b6339b1c5
zmeyer44 <zmmeyer44@gmail.com>
2023-11-16 05:44:37 -0500

    modal background color

--- app/_providers/modal/leaflet.tsx
+++ app/_providers/modal/leaflet.tsx
@@ -20,10 +20,10 @@ export function Leaflet({
     >
       <Drawer.Portal>
         <Drawer.Overlay className="fixed inset-0 bg-black/40" />
-        <Drawer.Content className="bg-zinc-100 flex flex-col rounded-t-[10px] h-[96%] mt-24 fixed bottom-0 left-0 right-0">
-          <div className="p-4 bg-white rounded-t-[10px] flex-1">
-            <div className="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-zinc-300 mb-8" />
-            <div className="max-w-md mx-auto">{children}</div>
+        <Drawer.Content className="fixed bottom-0 left-0 right-0 mt-24 flex h-[96%] flex-col rounded-t-[10px] bg-zinc-100">
+          <div className="flex-1 rounded-t-[10px] bg-background p-4">
+            <div className="mx-auto mb-8 h-1.5 w-12 flex-shrink-0 rounded-full bg-muted" />
+            <div className="mx-auto max-w-md">{children}</div>
           </div>
         </Drawer.Content>
       </Drawer.Portal>
