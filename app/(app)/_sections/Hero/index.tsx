91436982c33de67c73b6e86bf6cd08617335b408
zmeyer44 <zmmeyer44@gmail.com>
2023-12-05 23:44:44 -0500

    better hero page

--- app/(app)/_sections/Hero/index.tsx
+++ app/(app)/_sections/Hero/index.tsx
@@ -1,12 +1,19 @@
+import VideoPlayer from "@/components/videoPlayer";
+
 export default function HeroSection() {
   return (
-    <div className="flex flex-col px-5 pb-10 md:flex-row md:pb-20">
+    <div className="mx-auto flex max-w-7xl flex-col items-center gap-x-4 gap-y-3 px-5 pb-10 md:flex-row md:pb-20">
       <div className="flex-1">
         <h1 className="mt-14 text-5xl font-bold tracking-tight text-foreground sm:mt-10 md:text-6xl">
           Welcome to Flare, Lets Change the web
         </h1>
       </div>
-      <div className="h-[150px] w-5/12 shrink-0 bg-orange-500"></div>
+      <div className="w-full max-w-[450px] shrink-0 md:w-5/12">
+        <VideoPlayer
+          src="https://stream.mux.com/VZtzUzGRv02OhRnZCxcNg49OilvolTqdnFLEqBsTwaxU/low.mp4"
+          title="test"
+        />
+      </div>
     </div>
   );
 }
