e92065fbd2017f5e71d2c3f4ab80f8c0a511b17c
zmeyer44 <zmmeyer44@gmail.com>
2023-12-07 08:53:29 -0500

    removing unndeeded

--- app/(app)/_sections/Hero/index.tsx
+++ app/(app)/_sections/Hero/index.tsx
@@ -2,7 +2,7 @@ import VideoPlayer from "@/components/videoPlayer";
 
 export default function HeroSection() {
   return (
-    <div className="mx-auto flex max-w-7xl flex-col items-center gap-x-4 gap-y-3 px-5 pb-10 md:flex-row md:pb-20">
+    <div className="mx-auto flex max-w-7xl flex-col items-center gap-x-4 gap-y-5 px-5 pb-10 md:flex-row md:pb-20">
       <div className="flex-1">
         <h1 className="mt-14 text-5xl font-bold tracking-tight text-foreground sm:mt-10 md:text-6xl">
           Welcome to Flare, Lets Change the web
--- app/(app)/market/[market]/layout.tsx
+++
@@ -1,3 +0,0 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div className="mx-auto max-w-5xl py-4 md:px-4">{children}</div>;
-}
