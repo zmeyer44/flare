bf87489e0ecd073a329365ecfeff984e55552624
zmeyer44 <zmmeyer44@gmail.com>
2023-12-13 17:16:21 -0500

    testing

--- app/(app)/_sections/Hero/index.tsx
+++ app/(app)/_sections/Hero/index.tsx
@@ -4,8 +4,9 @@ export default function HeroSection() {
   return (
     <div className="mx-auto flex max-w-7xl flex-col items-center gap-x-4 gap-y-5 px-5 pb-10 md:flex-row md:pb-20">
       <div className="flex-1 md:px-6">
-        <h1 className="font-main mt-14 text-5xl font-semibold text-foreground sm:mt-10 lg:text-6xl">
-          Welcome to Flare, Lets Change the web
+        <h1 className="font-main mt-14 text-6xl font-semibold leading-6 text-foreground sm:mt-10 md:text-6xl lg:text-7xl">
+          Everything you need
+          <span className="mt-1 block">in one wallet</span>
         </h1>
       </div>
       <div className="aspect-video max-h-[350px] w-full max-w-[450px] shrink-0 pt-4 md:w-5/12 md:pt-6">
