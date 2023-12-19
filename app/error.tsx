6e98e69bd68d94ce15db2e78f9731cca9bd67ff9
zmeyer44 <zmmeyer44@gmail.com>
2023-12-19 06:04:25 -0500

    testing with loading and error

---
+++ app/error.tsx
@@ -0,0 +1,3 @@
+export default function Error() {
+  return <div className="h-full w-full bg-background"></div>;
+}
---
+++ app/loading.tsx
@@ -0,0 +1,3 @@
+export default function Loading() {
+  return <div className="h-full w-full bg-background"></div>;
+}
