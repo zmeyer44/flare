db11ed2421d5ee69fa92529e905586e7fd4e7534
zmeyer44 <zmmeyer44@gmail.com>
2023-12-20 09:46:59 -0500

    cron fix

--- app/api/crons/search-sync/route.ts
+++ app/api/crons/search-sync/route.ts
@@ -3,8 +3,7 @@ import prisma from "@/lib/prisma";
 import { updateVideo } from "@/lib/server-actions/search/algolia";
 
 async function handler() {
-  const Year = new Date(Date.now() - 1000 * 60 * 60 * 24 * 365);
-  const FiveMinsAgo = new Date(Date.now() - 1000 * 60 * 5);
+  const FiveMinsAgo = new Date(Date.now() - 1000 * 60 * 6);
   const recentUploads = await prisma.video.findMany({
     where: {
       OR: [
@@ -15,7 +14,7 @@ async function handler() {
         },
         {
           createdAt: {
-            gt: Year,
+            gt: FiveMinsAgo,
           },
         },
       ],
