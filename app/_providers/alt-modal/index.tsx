47e6ee4f8949d756744b9c8406d7d878f53fd693
zmeyer44 <zmmeyer44@gmail.com>
2023-12-22 10:14:39 -0500

    close nested post

--- app/_providers/alt-modal/index.tsx
+++ app/_providers/alt-modal/index.tsx
@@ -14,7 +14,11 @@ export function MyDrawer() {
         <Drawer.Overlay className="fixed inset-0 bg-black/40" />
         <Drawer.Content className="fixed bottom-0 left-0 right-0 mt-24 flex h-full max-h-[96%] flex-col rounded-t-[10px] bg-gray-100">
           <Leaflet openNested={() => setShowNested(true)} />
-          <Drawer.Root open={showNested} nested={true}>
+          <Drawer.Root
+            open={showNested}
+            nested={true}
+            onClose={() => setTimeout(() => setShowNested(true), 300)}
+          >
             <Drawer.Portal>
               <Drawer.Overlay className="fixed inset-0 bg-black/40" />
               <Drawer.Content className="fixed bottom-0 left-0 right-0 mt-24 flex h-full max-h-[94%] flex-col rounded-t-[10px] bg-gray-100">
