621b49ae7189dd2e58d4bcd8464f9c7ea85c9d62
zmeyer44 <zmmeyer44@gmail.com>
2023-12-24 10:00:36 -0600

    show full on preview

--- components/videoPlayer/layout.tsx
+++ components/videoPlayer/layout.tsx
@@ -21,7 +21,7 @@ export function VideoLayout({ thumbnails }: VideoLayoutProps) {
     <>
       <Gestures />
       <Captions
-        className={`${captionStyles.captions} absolute inset-0 bottom-2 z-10 select-none break-words opacity-0 transition-[opacity,bottom] duration-300 media-captions:opacity-100 media-controls:bottom-[85px] media-preview:opacity-0`}
+        className={`${captionStyles.captions} absolute inset-0 bottom-2 z-10 select-none break-words opacity-0 transition-[opacity,bottom] duration-300 group-hover:opacity-100 media-captions:opacity-100 media-controls:bottom-[85px] media-preview:opacity-0`}
       />
 
       <Controls.Root className="absolute inset-0 z-10 flex h-full w-full flex-col bg-gradient-to-t from-background/90 via-transparent to-transparent opacity-0 transition-opacity  group-hover:opacity-70 group-hover:delay-1000 media-controls:opacity-100">
