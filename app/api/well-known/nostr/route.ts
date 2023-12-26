5073f0ae7fc8b6d14474910d0c4cabcedb1dd497
zmeyer44 <zmmeyer44@gmail.com>
2023-12-25 20:25:39 -0600

    emoji np-05

--- app/api/well-known/nostr/route.ts
+++ app/api/well-known/nostr/route.ts
@@ -25,6 +25,7 @@ async function handler() {
     names: {
       _: "17717ad4d20e2a425cda0a2195624a0a4a73c4f6975f16b1593fc87fa46f2d58",
       zach: "17717ad4d20e2a425cda0a2195624a0a4a73c4f6975f16b1593fc87fa46f2d58",
+      "🔥": "17717ad4d20e2a425cda0a2195624a0a4a73c4f6975f16b1593fc87fa46f2d58",
     },
   });
   response.headers.set("Content-Type", "application/json");
