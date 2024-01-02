700e48027f0ae9ccc44ff68dcbac758aee86ee07
zmeyer44 <zmmeyer44@gmail.com>
2024-01-02 10:00:18 -0500

    fully functional

--- app/(app)/feed/Feed.tsx
+++ app/(app)/feed/Feed.tsx
@@ -9,7 +9,7 @@ import {
   SectionTitle,
 } from "@/containers/pageSection";
 import useEvents from "@/lib/hooks/useEvents";
-import { VideoCardLoading } from "@/components/cards/videoCard";
+import VideoCard, { VideoCardLoading } from "@/components/cards/videoCard";
 import type { NDKKind } from "@nostr-dev-kit/ndk";
 import { getTagValues } from "@/lib/nostr/utils";
 import { uniqBy } from "ramda";
@@ -18,12 +18,11 @@ export default function Feed() {
   const { events } = useEvents({
     filter: {
       kinds: [34235 as NDKKind],
-      limit: 1,
     },
   });
 
   const processedEvents = uniqBy((e) => getTagValues("title", e.tags), events);
-  if (false && processedEvents.length) {
+  if (processedEvents.length) {
     return (
       <Section className="px-5">
         <SectionHeader>
@@ -32,8 +31,8 @@ export default function Feed() {
           </SectionTitle>
         </SectionHeader>
         <SectionContent className="mx-auto">
-          <ul className="md-feed-cols  gap-4">
-            {/* {processedEvents.map((e) => {
+          <ul className="md-feed-cols gap-4">
+            {processedEvents.map((e) => {
               return (
                 <li key={e.id}>
                   <Link href={`/w/${e.encode()}`}>
@@ -41,7 +40,7 @@ export default function Feed() {
                   </Link>
                 </li>
               );
-            })} */}
+            })}
           </ul>
         </SectionContent>
       </Section>
@@ -51,23 +50,10 @@ export default function Feed() {
     <Section className="px-5">
       <SectionHeader>
         <SectionTitle className="font-main text-2xl font-semibold sm:text-3xl">
-          Recent Uploads {processedEvents.length}
+          Recent Uploads
         </SectionTitle>
       </SectionHeader>
       <SectionContent className="md-feed-cols relative mx-auto gap-4">
-        <VideoCardLoading />
-        <VideoCardLoading />
-        <VideoCardLoading />
-        <VideoCardLoading />
-        <VideoCardLoading />
-        <VideoCardLoading />
-        <VideoCardLoading />
-        <VideoCardLoading />
-        <VideoCardLoading />
-        <VideoCardLoading />
-        <VideoCardLoading />
-        <VideoCardLoading />
-        <VideoCardLoading />
         <VideoCardLoading />
         <VideoCardLoading />
         <VideoCardLoading />
