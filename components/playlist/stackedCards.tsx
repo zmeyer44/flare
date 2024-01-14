b3f654841256fe492fb89352f4343b3de6687e73
zmeyer44 <zmmeyer44@gmail.com>
2024-01-14 13:31:39 +0000

    meking it a bit better

--- components/playlist/stackedCards.tsx
+++ components/playlist/stackedCards.tsx
@@ -75,9 +75,9 @@ export default function StackedPlaylistCard({
   }
 
   return (
-    <div className="overflow-x-hidden">
-      <div className="mb-2 flex items-center">
-        <h2 className="text-base font-semibold">{title}</h2>
+    <div className="flex-1 overflow-x-visible">
+      <div className="mb-2 flex w-full items-center overflow-hidden">
+        <h2 className="truncate text-base font-semibold">{title}</h2>
       </div>
       <div className="isolate flex items-end justify-start -space-x-2 pb-2">
         {videosLoading ? (
