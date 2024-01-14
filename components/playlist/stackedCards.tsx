efcfede1b87cb1c7af36d57b59f2de9b2806cf23
zmeyer44 <zmmeyer44@gmail.com>
2024-01-14 13:14:01 +0000

    testing

--- components/playlist/stackedCards.tsx
+++ components/playlist/stackedCards.tsx
@@ -75,7 +75,7 @@ export default function StackedPlaylistCard({
   }
 
   return (
-    <div className="-mb-10 overflow-visible overflow-x-hidden pb-10">
+    <div className="overflow-x-hidden">
       <div className="mb-2 flex items-center">
         <h2 className="text-base font-semibold">{title}</h2>
       </div>
