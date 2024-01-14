2e8b19bd022bdedff941566cc468f4f54f6d95f4
zmeyer44 <zmmeyer44@gmail.com>
2024-01-14 21:51:00 +0000

    test

--- app/wallet/page.tsx
+++ app/wallet/page.tsx
@@ -2,13 +2,63 @@ import Link from "next/link";
 import { formatNumber } from "@/lib/utils";
 import RotaryButton from "./_components/RotaryButton";
 import Logo from "@/assets/Logo";
+import {
+  RiFlashlightLine,
+  RiFileList3Line,
+  RiLoginCircleFill,
+  RiLogoutCircleRLine,
+  RiCashLine,
+} from "react-icons/ri";
+
+type Transaction = {
+  type: "lightning" | "ecash";
+  direction: "in" | "out";
+  amount: number;
+};
 
 export default function Page() {
+  const transactions: Transaction[] = [
+    {
+      amount: 123244,
+      direction: "in",
+      type: "ecash",
+    },
+    {
+      amount: 3443,
+      direction: "out",
+      type: "ecash",
+    },
+    {
+      amount: 93244,
+      direction: "in",
+      type: "lightning",
+    },
+    {
+      amount: 44,
+      direction: "in",
+      type: "lightning",
+    },
+    {
+      amount: 10000,
+      direction: "in",
+      type: "lightning",
+    },
+    {
+      amount: 2121,
+      direction: "out",
+      type: "lightning",
+    },
+    {
+      amount: 10033,
+      direction: "in",
+      type: "ecash",
+    },
+  ];
   return (
     <div className="flex h-screen w-screen scale-100 transform flex-col bg-gradient-to-bl from-gray-400 to-gray-500">
       <div className="flex h-[45vh] w-full p-4">
-        <div className="relative h-full w-full overflow-hidden rounded-[3rem] bg-gradient-to-bl from-zinc-800 to-zinc-950">
-          <div className="absolute inset-0 rounded-[3rem] border-b-2 border-r-2 border-gray-50 border-opacity-70"></div>
+        <div className="relative flex h-full w-full flex-col overflow-hidden rounded-[3rem] bg-gradient-to-bl from-zinc-800 to-zinc-950">
+          {/* <div className="absolute inset-0 rounded-[3rem] border-b-2 border-r-2 border-gray-50 border-opacity-70"></div> */}
           <div className="flex w-full items-center justify-between border-b border-orange-600 border-opacity-40 px-[3rem] py-[4px]">
             <Link
               href="/"
@@ -26,6 +76,36 @@ export default function Page() {
           <div className="center font-major-mono flex-col pt-14 text-primary">
             <h1 className="font-audiowide text-6xl">{formatNumber(403423)}</h1>
             <p className="font-main">balance</p>
+          </div>
+          <div className="flex w-full flex-1 flex-col overflow-hidden px-3 pb-3 font-mono">
+            <div className="flex items-center gap-x-1 text-primary">
+              <h3 className="font-normal">txns</h3>
+              <RiFileList3Line className="h-4 w-4" />
+            </div>
+            <ul className="mt-1 flex-1 space-y-2 overflow-y-auto rounded-xl rounded-b-[41px] border border-primary bg-orange-800/30 p-3 text-primary">
+              {transactions.map((e, idx) => (
+                <li key={idx} className="flex items-center gap-x-2">
+                  <div className="flex items-center gap-x-1">
+                    {e.direction === "in" ? (
+                      <RiLoginCircleFill className="h-4 w-4" />
+                    ) : (
+                      <RiLogoutCircleRLine className="h-4 w-4" />
+                    )}
+                    {e.type === "ecash" ? (
+                      <RiCashLine className="h-4 w-4" />
+                    ) : (
+                      <RiFlashlightLine className="h-4 w-4" />
+                    )}
+                  </div>
+                  <div className="h-0 flex-1 border-b-2 border-dotted border-primary/60"></div>
+                  <div className="flex items-center ">
+                    <p className="text-[14px] leading-none">
+                      {formatNumber(e.amount)}
+                    </p>
+                  </div>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
