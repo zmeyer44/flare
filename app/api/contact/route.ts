815efd1a0f8e3e89c16e437717a65e9c8d375b7b
zmeyer44 <zmmeyer44@gmail.com>
2024-01-23 19:13:47 +0000

    testing more

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -6,18 +6,32 @@ import NDK, {
 } from "@nostr-dev-kit/ndk";
 import { RELAYS } from "@/constants";
 import { unixTimeNowInSeconds } from "@/lib/nostr/dates";
+import { z } from "zod";
+
+const contactParse = z.object({
+  name: z.string(),
+  email: z.string(),
+  message: z.string(),
+});
 async function handler(req: NextRequest) {
-  const signer = NDKPrivateKeySigner.generate();
+  const signer = new NDKPrivateKeySigner(
+    process.env.TESTING_PRIVATE_KEY as string,
+  );
   const ndkInstance = new NDK({
     explicitRelayUrls: RELAYS,
     signer,
   });
-  await ndkInstance.connect();
+  await ndkInstance.connect(6000);
   ndkInstance.signer = signer;
+  const { email, name, message } = contactParse.parse(req.body);
 
   const event = new NDKEvent(ndkInstance, {
-    content: "TEST MESSAGE",
+    content: `Contact form submission: \n
+    Name: ${name} \n
+    Email: ${email}\n
+    Message: ${message}`,
     pubkey: (await signer.user()).pubkey,
+    kind: 4,
     tags: [
       ["p", "17717ad4d20e2a425cda0a2195624a0a4a73c4f6975f16b1593fc87fa46f2d58"],
     ],
